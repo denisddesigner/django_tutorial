2d5003f20694348be9e7206dbc313d46d4b63ab6
Denis M <denisddesigner@gmail.com>
2017-09-22 13:19:39 +0100

    404 error handling and namespacing

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse
 from .models import Question
+from django.shortcuts import get_object_or_404, render
 
 # Create your views here.
 
@@ -22,5 +23,11 @@ def results(request, question_id):
 
 def vote(request, question_id):
 	return HttpResponse("You're voting on question %s."% question_id)
+
+def detail(request, question_id):
+	question = get_object_or_404(Question, pk=question_id)
+	return render(request, 'polls/detail.html', {'question': question})
+
+
 
 
